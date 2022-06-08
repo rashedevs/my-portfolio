cb13f3316db9a74e36caf6886ce638aa6217705c
rashedevs <rasheduap2015@gmail.com>
2022-06-09 05:48:07 +0600

    images imported from assets

--- src/components/Home.js
+++ src/components/Home.js
@@ -21,7 +21,7 @@ const Home = () => {
         <div>
           <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
             My Resume
-            <span className="group-hover:rotate-45 duration-300">
+            <span className="group-hover:rotate-45 duration-100">
               <HiArrowNarrowRight className="ml-3 " />
             </span>
           </button>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,8 +1,18 @@
 import React from "react";
+import html from "../assets/html.png";
+import css from "../assets/css.png";
+import javascript from "../assets/javascript.png";
+import react from "../assets/react.png";
+import node from "../assets/node.png";
+import firebase from "../assets/firebase.png";
+import express from "../assets/express.png";
+import github from "../assets/github.png";
+import tailwind from "../assets/tailwind.png";
+import mongo from "../assets/mongo.png";
 
 const Skills = () => {
   return (
-    <div>
+    <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
       <h2>This is my skills.</h2>
     </div>
   );
