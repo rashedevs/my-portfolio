a2886dc810819b6f49f67668aa24ed435218f24e
rashedevs <rasheduap2015@gmail.com>
2022-06-09 08:01:06 +0600

    using getform post method for form submission

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,9 +4,13 @@ const Contact = () => {
   return (
     <div
       name="contact"
-      className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4"
+      className="w-full h-full bg-[#0a192f] flex justify-center items-center p-4"
     >
-      <form action="" className="flex flex-col max-w-[600px] w-full">
+      <form
+        method="POST"
+        action="https://getform.io/f/e10b563d-d3cc-4780-b147-c04e547238be"
+        className="flex flex-col max-w-[600px] w-full"
+      >
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-yellow-400 text-gray-300">
             Wanna Reach Me?
