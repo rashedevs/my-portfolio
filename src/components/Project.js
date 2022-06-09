d8cc8d1d5854620afbb7fa35ac352455b5e73158
rashedevs <rasheduap2015@gmail.com>
2022-06-10 00:07:06 +0600

    projects background and links added

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,10 @@
 import React from "react";
-import workImg from "../assets/workImg.jpeg";
+import metalik from "../assets/metalik.png";
+import carventory from "../assets/carventory.png";
+import momenta from "../assets/momenta.png";
+import bear from "../assets/bear.png";
+import clock from "../assets/clock.png";
+import convention from "../assets/convention.png";
 
 const Project = () => {
   return (
@@ -18,29 +23,43 @@ const Project = () => {
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-6">
           <div
-            style={{ backgroundImage: `url(${workImg})` }}
+            style={{ backgroundImage: `url(${metalik})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
                 Metalik
               </span>
+              <p className=" font-semibold text-yellow-400 tracking-wider">
+                A full-stack website
+              </p>
               <div className="pt-8 text-center">
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Details
+                <a target={`_blank`} href="https://metalik-ab578.web.app/">
+                  <button className="text-center rounded-lg px-4 py-2 m-2 bg-white text-gray-700 font-bold text-lg hover:bg-pink-600 hover:text-white">
+                    Live
                   </button>
                 </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
+                <a
+                  target={`_blank`}
+                  href="https://github.com/rashedevs/metalik-client"
+                >
+                  <button className="text-center rounded-lg px-4 py-2 m-2 bg-white text-gray-700 font-bold text-lg hover:bg-pink-600 hover:text-white">
+                    Client
+                  </button>
+                </a>
+                <a
+                  target={`_blank`}
+                  href="https://github.com/rashedevs/metalik-server"
+                >
+                  <button className="text-center rounded-lg px-4 py-2 m-2 bg-white text-gray-700 font-bold text-lg hover:bg-pink-600 hover:text-white">
+                    Server
                   </button>
                 </a>
               </div>
             </div>
           </div>
           <div
-            style={{ backgroundImage: `url(${workImg})` }}
+            style={{ backgroundImage: `url(${carventory})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
@@ -48,15 +67,29 @@ const Project = () => {
               <span className="text-2xl font-bold text-white tracking-wider">
                 Carventory
               </span>
+              <p className=" font-semibold text-yellow-400 tracking-wider">
+                A full-stack website
+              </p>
               <div className="pt-8 text-center">
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Details
+                <a target={`_blank`} href="https://carventory-581a5.web.app/">
+                  <button className="text-center rounded-lg px-4 py-2 m-2 bg-white text-gray-700 font-bold text-lg hover:bg-pink-600 hover:text-white">
+                    Live
                   </button>
                 </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
+                <a
+                  target={`_blank`}
+                  href="https://github.com/rashedevs/carventory-client"
+                >
+                  <button className="text-center rounded-lg px-4 py-2 m-2 bg-white text-gray-700 font-bold text-lg hover:bg-pink-600 hover:text-white">
+                    Client
+                  </button>
+                </a>
+                <a
+                  target={`_blank`}
+                  href="https://github.com/rashedevs/carventory-server"
+                >
+                  <button className="text-center rounded-lg px-4 py-2 m-2 bg-white text-gray-700 font-bold text-lg hover:bg-pink-600 hover:text-white">
+                    Server
                   </button>
                 </a>
               </div>
@@ -64,7 +97,7 @@ const Project = () => {
           </div>
           {/* Grid Item */}
           <div
-            style={{ backgroundImage: `url(${workImg})` }}
+            style={{ backgroundImage: `url(${momenta})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
@@ -72,69 +105,31 @@ const Project = () => {
               <span className="text-2xl font-bold text-white tracking-wider">
                 Momenta Photography
               </span>
+              <p className=" font-semibold text-yellow-400 tracking-wider">
+                A service provider website
+              </p>
               <div className="pt-8 text-center">
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Details
+                <a
+                  target={`_blank`}
+                  href="https://momenta-photography.web.app/"
+                >
+                  <button className="text-center rounded-lg px-4 py-2 m-2 bg-white text-gray-700 font-bold text-lg hover:bg-pink-600 hover:text-white">
+                    Live
                   </button>
                 </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
+                <a
+                  target={`_blank`}
+                  href="https://github.com/rashedevs/momenta-photography"
+                >
+                  <button className="text-center rounded-lg px-4 py-2 m-2 bg-white text-gray-700 font-bold text-lg hover:bg-pink-600 hover:text-white">
+                    Github Code
                   </button>
                 </a>
               </div>
             </div>
           </div>
           <div
-            style={{ backgroundImage: `url(${workImg})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Teddy Bear
-              </span>
-              <div className="pt-8 text-center">
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Details
-                  </button>
-                </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          {/* Grid Item */}
-          <div
-            style={{ backgroundImage: `url(${workImg})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                NextGen Mobile
-              </span>
-              <div className="pt-8 text-center">
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Details
-                  </button>
-                </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
-            style={{ backgroundImage: `url(${workImg})` }}
+            style={{ backgroundImage: `url(${clock})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
@@ -142,15 +137,89 @@ const Project = () => {
               <span className="text-2xl font-bold text-white tracking-wider">
                 Clock Zone
               </span>
+              <p className=" font-semibold text-yellow-400 tracking-wider">
+                A react.js website
+              </p>
               <div className="pt-8 text-center">
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Details
+                <a
+                  target={`_blank`}
+                  href="https://clock-zone-by-rashed.netlify.app/"
+                >
+                  <button className="text-center rounded-lg px-4 py-2 m-2 bg-white text-gray-700 font-bold text-lg hover:bg-pink-600 hover:text-white">
+                    Live
                   </button>
                 </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
+                <a
+                  target={`_blank`}
+                  href="https://github.com/rashedevs/clock-zone"
+                >
+                  <button className="text-center rounded-lg px-4 py-2 m-2 bg-white text-gray-700 font-bold text-lg hover:bg-pink-600 hover:text-white">
+                    Github Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${bear})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Bear World
+              </span>
+              <p className=" font-semibold text-yellow-400 tracking-wider">
+                A responsive website
+              </p>
+              <div className="pt-8 text-center">
+                <a
+                  target={`_blank`}
+                  href="https://bear-world-by-rashed.netlify.app/"
+                >
+                  <button className="text-center rounded-lg px-4 py-2 m-2 bg-white text-gray-700 font-bold text-lg hover:bg-pink-600 hover:text-white">
+                    Live
+                  </button>
+                </a>
+                <a
+                  target={`_blank`}
+                  href="https://github.com/rashedevs/bear-world"
+                >
+                  <button className="text-center rounded-lg px-4 py-2 m-2 bg-white text-gray-700 font-bold text-lg hover:bg-pink-600 hover:text-white">
+                    Github Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${convention})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Mirzapur Convention
+              </span>
+              <p className=" font-semibold text-yellow-400 tracking-wider">
+                A responsive website
+              </p>
+              <div className="pt-8 text-center">
+                <a
+                  target={`_blank`}
+                  href="https://convention-center-rashed.netlify.app/"
+                >
+                  <button className="text-center rounded-lg px-4 py-2 m-2 bg-white text-gray-700 font-bold text-lg hover:bg-pink-600 hover:text-white">
+                    Live
+                  </button>
+                </a>
+                <a
+                  target={`_blank`}
+                  href="https://github.com/rashedevs/convention-center"
+                >
+                  <button className="text-center rounded-lg px-4 py-2 m-2 bg-white text-gray-700 font-bold text-lg hover:bg-pink-600 hover:text-white">
+                    Github Code
                   </button>
                 </a>
               </div>
